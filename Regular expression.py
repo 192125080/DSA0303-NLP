addecaaecc7c66007f1b831964638ebac8463f51
192125080 <113422967+192125080@users.noreply.github.com>
2023-10-16 12:19:05 +0530

    Add files via upload

--- Regular expression.py
+++ Regular expression.py
@@ -1,26 +1,30 @@
 import re
 
-def tokenize(text):
-    # Tokenize text using regular expression
-    tokens = re.findall(r'\b\w+\b', text)
-    return tokens
-
-def find_emails(text):
-    # Find email addresses using regular expression
-    emails = re.findall(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b', text)
-    return emails
-
 def find_dates(text):
-    # Find dates using regular expression (simple example)
-    dates = re.findall(r'\d{1,2}/\d{1,2}/\d{2,4}', text)
+    pattern = r'\d{2}/\d{2}/\d{4}'
+    dates = re.findall(pattern, text)
     return dates
 
-# Example Usage
-text = "Hello, my email is user@example.com. I'll meet you on 12/25/2023."
-tokens = tokenize(text)
-emails = find_emails(text)
-dates = find_dates(text)
+def find_emails(text):
+    pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+    emails = re.findall(pattern, text, re.IGNORECASE)
+    return emails
 
-print("Tokens:", tokens)
-print("Emails:", emails)
-print("Dates:", dates)
+def main():
+    sample_text = """
+    John Doe was born on 25/12/1985. His email is john.doe@example.com.
+    Jane Doe's birthday is on 03/09/1990. You can reach her at jane_doe@gmail.com.
+    """
+
+    print("Dates found:")
+    dates = find_dates(sample_text)
+    for date in dates:
+        print(date)
+
+    print("\nEmails found:")
+    emails = find_emails(sample_text)
+    for email in emails:
+        print(email)
+
+if __name__ == "__main__":
+    main()
